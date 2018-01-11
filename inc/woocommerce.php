a35f98d52780a8f08d42cf70290e34de0129fe6d
Fritz <fre.healy@gmail.com>
2018-01-11 10:21:58 -0500

    removing reviews custom tab styling account page

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -64,15 +64,10 @@ add_filter( 'woocommerce_return_to_shop_redirect', 'wc_empty_cart_redirect_url' 
 /**
  * Each tab is an array containing title, callback and priority.
  * */
-if(!function_exists('bella_add_tabs')&&!function_exists('bella_whats_tab')&&!function_exists('bella_reviews_tab')){
+if(!function_exists('bella_add_tabs')&&!function_exists('bella_whats_tab')){
     add_filter('woocommerce_product_tabs','bella_add_tabs');
     function bella_add_tabs($array){
-		if(!isset($array['bella_reviews'])&&!isset($array['bella_whats'])):
-			$array['bella_reviews'] = array(
-				'title'=>'Reviews',
-				'priority'=>15,
-				'callback'=>'bella_reviews_tab'
-			);
+		if(!isset($array['bella_whats'])):
 			$array['bella_whats'] = array(
 				'title'=>'What\'s In The Box?',
 				'priority'=>15,
@@ -86,25 +81,6 @@ if(!function_exists('bella_add_tabs')&&!function_exists('bella_whats_tab')&&!fun
 		if($whats):
 			echo $whats;
 		endif;
-	}
-	function bella_reviews_tab(){
-		$args = array(
-			'post_id'=>get_the_ID(),
-			'status'=>'approve',
-			'orderby'=>'date',
-		);
-		 // The Query
-		 $comments_query = new WP_Comment_Query;
-		 $comments = $comments_query->query( $args );
-		 
-		 // Comment Loop
-		 if ( $comments ) {
-			 foreach ( $comments as $comment ) {
-				 echo '<p>' . $comment->comment_content . '</p>';
-			 }
-		 } else {
-			 echo 'No reviews found.';
-		 }
 	}
 }
 
